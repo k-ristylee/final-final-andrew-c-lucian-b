2c3910a709f67612e93d57dcdc4d7df8e73084f2
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-20 11:32:54 -0400

    May 20: added images for different grandma levels
    - normal grandma, grandma with cane, grandma with wheelchair, grandma with monster truck
    - idea is that you can level up and upgrade into different grandma in game

---
+++ Grandma.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Grandma here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Grandma extends Actor
+{
+    /**
+     * Act - do whatever the Grandma wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
