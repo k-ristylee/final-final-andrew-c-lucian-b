d9416fbcff762078a9a8264c3743ac87f7d6926a
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-20 11:41:31 -0400

    May 20: spawn moving grandma
    - created grandma subclass
    - spawning grandma and allowing grandma to move in response to arrow keys

--- Grandma.java
+++ Grandma.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Grandma here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Lucian and Andrew
+ * @May 20, 2026
  */
 public class Grandma extends Actor
 {
@@ -12,8 +12,20 @@ public class Grandma extends Actor
      * Act - do whatever the Grandma wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("left")){
+            setLocation(getX() - 3, getY());
+        }
+        else if(Greenfoot.isKeyDown("right")){
+            setLocation(getX() + 3, getY());
+        }
+        else if(Greenfoot.isKeyDown("up")){
+            setLocation(getX(), getY() - 3);
+        }
+        else if(Greenfoot.isKeyDown("down")){
+            setLocation(getX(), getY() + 3);
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -2,6 +2,9 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
+        
+        Grandma grandma = new Grandma();
+        addObject(grandma, 300, 200);
     }
 }
